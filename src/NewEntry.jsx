3d46529a6ce2a2eac185f9d5b39e05e2c773fddd
Rickie Natividad <rickienatividad85@gmail.com>
2022-11-09 12:26:30 -0500

    Adds functionality to new entry form

--- src/NewEntry.jsx
+++ src/NewEntry.jsx
@@ -1,17 +1,34 @@
+import axios from "axios";
 export function NewEntry() {
   const userId = localStorage.getItem("userId");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/entries.json", params)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+      });
+  };
   return (
     <div>
       <h2>Entry Details</h2>
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <div>user id</div>
-        <input type="text" defaultValue={userId} />
+        <input type="text" defaultValue={userId} name="user_id" />
+        <div>date</div>
+        <input type="date" name="date" />
         <div>location</div>
-        <input type="text" />
+        <input type="text" name="location" />
         <div>weather</div>
-        <input type="text" />
+        <input type="text" name="weather" />
         <div>notes</div>
-        <input type="textarea" cols="10" rows="20" />
+        <input type="textarea" cols="10" rows="20" name="notes" />
         <div>
           <button>Submit</button>
         </div>
